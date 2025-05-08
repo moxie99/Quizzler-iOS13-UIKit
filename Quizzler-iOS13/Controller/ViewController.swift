738cef2710677ef2aba01909a1ea2cb34ef41c36
Adeolu David <adeolusegun99@gmail.com>
2025-05-08 14:58:09 +0100

    Initial Commit

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questonLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBank = QuizBank()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        updateQuestion()
+    }
+
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        if quizBank.checkAnswer(userAnswer) {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        quizBank.quizSwitch()
+      
+        Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateQuestion), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateQuestion() {
+        questonLabel.text = quizBank.getQuestionText()
+        progressBar.progress = quizBank.getProgress()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+}
+
